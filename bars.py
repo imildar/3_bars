0e6abe6bbb5822d1b71107585bc1ad599c0dbf51
imildar <30859071+imildar@users.noreply.github.com>
2017-10-09 16:36:56 +0500

    Update bars.py

--- bars.py
+++ bars.py
@@ -1,6 +1,6 @@
-import json
 import requests
 import math
+
 
 def load_data(file):
     return requests.get(file).json()
@@ -27,18 +27,31 @@ def get_closest_bar(data, longitude, latitude):
     for bar in bars:
         bar_name_place[bar['properties']['Attributes']['Name']] = bar['geometry']['coordinates']
         
-    long, lat = input('Введите координаты через пробел (долгота, широта): ').split()
-    long = float(long)
-    lat = float(lat)
+    long = float(longitude)
+    lat = float(latitude)
     closest = 10000
     for name,c in bar_name_place.items():
         dist = math.sqrt((c[0] - long)**2 + (c[1] - lat)**2)
         if dist < closest:
             closest = dist
-            near_bar = {name: closest}
+            near_bar = {name: bar_name_place[name]}
     return near_bar
 
 
 url = 'https://devman.org/fshare/1503831681/4/'
 
 bars = load_data(url)['features']
+
+if __name__ == '__main__':
+    input_number = int(input('''Чтобы найти бар с наибольшей вместимостью, нажмите 1\n\
+Чтобы найти бар с наименьшей вместимостью, нажмите 2\n\
+Чтобый найти ближайший бар, нажмите 3\n'''))
+
+    if  input_number == 1:
+        print ('Самый большой бар -',get_biggest_bar(bars))
+    if input_number == 2:
+        print ('Самый маленький бар-',get_smallest_bar(bars))    
+    if input_number == 3:
+        longitude=float(input("Введитете долготу: "))
+        latitude=float(input("Введитете широту: "))
+        print ("Ближайший бар - ",get_closest_bar(bars, longitude, latitude)) 
